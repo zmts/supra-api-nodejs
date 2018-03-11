60d5301172070acc5b0efa5c395d5e97c24d4ef8
Sasha Zmts <sa.zmts@gmail.com>
2018-03-11 19:06:26 +0200

    init knex migrations

---
+++ knexfile.js
@@ -0,0 +1,7 @@
+const config = require('./config')
+
+module.exports = {
+  development: {
+    ...config.knex.dev
+  }
+}
---
+++ migrations/20180311190528_setup.js
@@ -0,0 +1,7 @@
+exports.up = function(knex, Promise) {
+
+};
+
+exports.down = function(knex, Promise) {
+
+};
