cd869e7443dcf2e79c6ecbda9ce2865c5f8d42ff
Sasha Zmts <sa.zmts@gmail.com>
2018-03-15 21:23:06 +0200

    minor

--- util/required.js
+++ util/required.js
@@ -1,4 +1,4 @@
-const ErrorWrapper = require('../util/Error')
+const ErrorWrapper = require('./Error')
 
 module.exports = paramName => {
   throw new ErrorWrapper(`${paramName || 'Required'} parameter not supplied`, 500)
--- util/typecheck.js
+++ util/typecheck.js
@@ -1,23 +1,21 @@
-const _ = require('lodash')
-const ErrorWrapper = require('../util/Error')
+const ErrorWrapper = require('./Error')
 
-module.exports = (actualArguments, schema) => {
+module.exports = (args, schema) => {
   /**
    * check required arguments
    */
   let schemaRequiredCount = schema.filter(item => item.required).length
-  if (actualArguments.length < schemaRequiredCount) {
+  if (args.length < schemaRequiredCount) {
     throw new ErrorWrapper('Required parameters not supplied', 500)
   }
-
   /**
    * check arguments type
    */
-  _.forEach(actualArguments, (actualArgumentsItem, actualArgumentsIndex) => {
+  Array.from(args).forEach((actualArgumentsItem, actualArgsIndex) => {
     let actualArgumentType = typeof actualArgumentsItem
 
-    if (actualArgumentType !== schema[actualArgumentsIndex].type) {
-      throw new ErrorWrapper(`>> ${schema[actualArgumentsIndex].name} << parameter wrong type`, 500)
+    if (actualArgumentType !== schema[actualArgsIndex].type) {
+      throw new ErrorWrapper(`>> ${schema[actualArgsIndex].name} << parameter wrong type`, 500)
     }
   })
 }
