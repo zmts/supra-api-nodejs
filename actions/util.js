b4a33a98a9073a41d94752012b918b89ec85a87d
Sasha Zmts <sa.zmts@gmail.com>
2018-03-17 11:49:19 +0200

    minor

--- actions/util.js
+++ actions/util.js
@@ -1,5 +1,10 @@
+const ErrorWrapper = require('../util/Error')
+
 module.exports.actionRunner = (action = global.required('action')) => {
   return (req, res, next) => {
+    if (!action.hasOwnProperty('run')) {
+      throw new ErrorWrapper(`'run' method not declared in invoked '${action.name}' action`, 500)
+    }
     return action.run(req, res, next)
   }
 }
