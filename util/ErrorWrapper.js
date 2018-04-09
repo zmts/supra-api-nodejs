d27184a7e9def56c980792ebc6fe3a005c1f4074
Sasha Zmts <sa.zmts@gmail.com>
2018-04-09 17:13:11 +0300

    upd ErrorWrapper

--- util/ErrorWrapper.js
+++ util/ErrorWrapper.js
@@ -1,7 +1,6 @@
 class ErrorWrapper extends Error {
   constructor (message, status) {
     super()
-    this.stack = new Error().stack
     this.message = message
     this.status = status || 500
   }
