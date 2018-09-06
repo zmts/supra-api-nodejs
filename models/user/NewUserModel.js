7b3d3a2d32fc88aaf86d244ede9dd8f407cd2f52
Sasha Zmts <sa.zmts@gmail.com>
2018-09-06 15:00:54 +0300

    user model

--- models/user/NewUserModel.js
+++ models/user/NewUserModel.js
@@ -1,1 +1,14 @@
-// TODO
+const BaseModel = require('../BaseModel')
+
+class NewUserModel extends BaseModel {
+  get schema () {
+    return {
+      name: this.joi.string().min(3).max(50),
+      username: this.joi.string().min(3).max(25).required(),
+      password: this.joi.string().required(), // stores in DB as passwordHash,
+      email: this.joi.string().email().min(6).max(30).required()
+    }
+  }
+}
+
+module.exports = NewUserModel
--- models/user/UpdateUserModel.js
+++ models/user/UpdateUserModel.js
@@ -1,1 +1,12 @@
-// TODO
+const BaseModel = require('../BaseModel')
+
+class UpdateUserModel extends BaseModel {
+  get schema () {
+    return {
+      name: this.joi.string().min(3).max(50),
+      username: this.joi.string().min(3).max(25).required()
+    }
+  }
+}
+
+module.exports = UpdateUserModel
