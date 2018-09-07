558b8fb4d9bb947706b73132ecb211a01970f037
Sasha Zmts <sa.zmts@gmail.com>
2018-09-07 22:37:32 +0300

    upd BaseModel

--- models/BaseModel.js
+++ models/BaseModel.js
@@ -1,22 +1,23 @@
-const joi = require('joi')
-const joiToJsonSchema = require('joi-to-json-schema')
+const Joi = require('joi')
+const JoiToJsonSchema = require('joi-to-json-schema')
 
 class BaseModel {
   constructor (dataObj) {
-    __typecheck(dataObj, __type.object, true)
-
     /**
-     * if front-end needs model schema break model initialization and return schema json
+     * if no input data break model initialization and return schema json
      */
-    if (dataObj.isSchema) return this.schemaToJson
+    if (!dataObj) return this.schemaToJson
 
     const validationResult = this.joi.validate(dataObj, this.schema)
     if (validationResult.error) { throw new Error(validationResult.error) }
-    this.buildModelProps(dataObj, this.schema, this)
+
+    if (!this.isInitialized) {
+      buildModelProps(dataObj, this.schema, this)
+    }
   }
 
   get joi () {
-    return joi
+    return Joi
   }
 
   get schema () {
@@ -24,28 +25,34 @@ class BaseModel {
   }
 
   get schemaToJson () {
-    return joiToJsonSchema(this.joi.object({ ...this.schema }))
+    return JoiToJsonSchema(this.joi.object({ ...this.schema }))
   }
+}
 
-  buildModelProps (dataObj, schema, context) {
-    __typecheck(dataObj, __type.object, true)
-    __typecheck(schema, __type.object, true)
-    __typecheck(context, __type.object, true)
+function buildModelProps (dataObj, schema, context) {
+  __typecheck(dataObj, __type.object, true)
+  __typecheck(schema, __type.object, true)
+  __typecheck(context, __type.object, true)
 
-    Object.keys(dataObj).forEach(propName => {
-      Object.defineProperty(context, propName, {
-        get: () => dataObj[propName],
-        set: value => {
-          const isValidValue = this.joi.validate(value, schema[propName])
-          if (isValidValue.error) { throw new Error(isValidValue.error) }
-          dataObj[propName] = value
-        },
-        enumerable: true,
-        configurable: false
-      })
+  Object.keys(dataObj).forEach(propName => {
+    Object.defineProperty(context, propName, {
+      get: () => dataObj[propName],
+      set: value => {
+        const isValidValue = Joi.validate(value, schema[propName])
+        if (isValidValue.error) { throw new Error(isValidValue.error) }
+        dataObj[propName] = value
+      },
+      enumerable: true,
+      configurable: false
     })
-    return context
-  }
+  })
+
+  Object.defineProperty(context.__proto__, 'isInitialized', {
+    get: () => true,
+    configurable: false
+  })
+
+  return context
 }
 
 module.exports = BaseModel
