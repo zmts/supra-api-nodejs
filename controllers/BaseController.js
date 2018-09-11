5c8c5984cbbda9bb674d03d1ae387dbbca9cb180
Sasha Zmts <sa.zmts@gmail.com>
2018-09-11 23:09:32 +0300

    upd BaseController

--- controllers/BaseController.js
+++ controllers/BaseController.js
@@ -7,18 +7,19 @@ class BaseController {
   static actionRunner (action) {
     __typecheck(action, 'Function', true)
 
+    if (!action.hasOwnProperty('accessTag')) {
+      throw new ErrorWrapper({ message: `'accessTag' getter not declared in invoked '${action.name}' action`, status: 500 })
+    }
+
+    if (!action.hasOwnProperty('run')) {
+      throw new ErrorWrapper({ message: `'run' method not declared in invoked '${action.name}' action`, status: 500 })
+    }
+
     return async (req, res, next) => {
       __typecheck(req, 'Object', true)
       __typecheck(res, 'Object', true)
       __typecheck(next, 'Function', true)
 
-      if (!action.hasOwnProperty('accessTag')) {
-        throw new ErrorWrapper({ message: `'accessTag' getter declared in invoked '${action.name}' action`, status: 500 })
-      }
-
-      if (!action.hasOwnProperty('run')) {
-        throw new ErrorWrapper({ message: `'run' method not declared in invoked '${action.name}' action`, status: 500 })
-      }
       if (req.query.schema) {
         return res.json(action.jsonSchema)
       }
