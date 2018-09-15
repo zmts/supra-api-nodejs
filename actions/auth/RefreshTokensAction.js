6886f03ca43d295b3a7baf897dcda2a88cb317b0
Sasha Zmts <sa.zmts@gmail.com>
2018-09-15 19:04:37 +0300

    upd RefreshTokensAction

--- actions/auth/RefreshTokensAction.js
+++ actions/auth/RefreshTokensAction.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 const BaseAction = require('../BaseAction')
 const UserDAO = require('../../dao/UserDAO')
 const { parseTokenService, makeAccessTokenService, makeRefreshTokenService, findAndVerifyRefreshToken } = require('../../services/auth')
-const errorCodes = require('../../config').errorCodes
+const { errorCodes } = require('../../config')
 
 class RefreshTokensAction extends BaseAction {
   static get accessTag () {
@@ -12,46 +12,37 @@ class RefreshTokensAction extends BaseAction {
 
   static get validationRules () {
     return {
-      ...this.baseValidationRules,
       body: Joi.object().keys({
         refreshToken: Joi.string().required()
       })
     }
   }
 
-  static run (req, res, next) {
-    let reqRefreshToken = req.body['refreshToken']
-    let refreshTokenTimestamp = reqRefreshToken.split('::')[0]
-
+  static async run (req, res, next) {
+    const reqRefreshToken = req.body['refreshToken']
+    const refreshTokenTimestamp = reqRefreshToken.split('::')[0]
+    const refreshToken = reqRefreshToken.split('::')[1]
+    const responseData = { accessToken: '', refreshToken: '' }
     let userEntity = {}
-    let responseData = { accessToken: '', refreshToken: '' }
 
-    this.init(req, this.validationRules, this.accessTag)
-      .then(() => {
-        const refreshToken = reqRefreshToken.split('::')[1]
-        return parseTokenService(refreshToken) // parse refresh token data (taken from request)
-      })
-      .then(refreshTokenData => UserDAO.BaseGetById(+refreshTokenData.sub)) // get user entity from DB
-      .then(user => {
-        userEntity = user
-        return findAndVerifyRefreshToken(userEntity, reqRefreshToken)
-      })
-      .then(() => UserDAO.RemoveRefreshToken(+userEntity.id, refreshTokenTimestamp)) // remove old refresh token
-      .then(() => makeRefreshTokenService(userEntity)) // make new refresh token
-      .then(newRefreshToken => {
-        responseData.refreshToken = newRefreshToken
-        const newRefreshTokenTimestamp = newRefreshToken.split('::')[0]
-        return UserDAO.AddRefreshTokenProcess(userEntity, { timestamp: newRefreshTokenTimestamp, refreshToken: newRefreshToken }) // store new refresh token to DB
-      })
-      .then(() => makeAccessTokenService(userEntity))
-      .then(accessToken => (responseData.accessToken = accessToken))
-      .then(() => res.json(this.resJson({ data: responseData })))
-      .catch(error => {
-        next(error)
-        if (error.code === errorCodes.TOKEN_EXPIRED.code) {
-          return UserDAO.RemoveRefreshToken(+userEntity.id, refreshTokenTimestamp) // remove refresh token in not valid
-        }
-      })
+    try {
+      const refreshTokenData = await parseTokenService(refreshToken) // parse refresh token data (taken from request)
+      userEntity = await UserDAO.BaseGetById(+refreshTokenData.sub) // get user entity from DB
+      await findAndVerifyRefreshToken(userEntity, reqRefreshToken)
+      await UserDAO.RemoveRefreshToken(+userEntity.id, refreshTokenTimestamp) // remove old refresh token
+      const newRefreshToken = await makeRefreshTokenService(userEntity) // make new refresh token
+      responseData.refreshToken = newRefreshToken
+      const newRefreshTokenTimestamp = newRefreshToken.split('::')[0]
+      await UserDAO.AddRefreshTokenProcess(userEntity, { timestamp: newRefreshTokenTimestamp, refreshToken: newRefreshToken }) // store new refresh token to DB
+      const accessToken = await makeAccessTokenService(userEntity)
+      responseData.accessToken = accessToken
+      res.json(this.resJson({ data: responseData }))
+    } catch (error) {
+      next(error)
+      if (error.code === errorCodes.TOKEN_EXPIRED.code) {
+        return UserDAO.RemoveRefreshToken(+userEntity.id, refreshTokenTimestamp) // remove refresh token in not valid
+      }
+    }
   }
 }
 
