7792451169cea195c1f205b869c76607fa63fd28
Sasha Zmts <sa.zmts@gmail.com>
2019-03-21 10:58:50 +0200

    delete __rootdir variable

--- globals.js
+++ globals.js
@@ -16,6 +16,5 @@ const logger = require('./util/logger')
 module.exports = () => {
   global.__typecheck = typecheck
   global.__type = type
-  global.__rootdir = __dirname
   global.__logger = logger
 }
