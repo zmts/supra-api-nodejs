c7cdc941e3aac0787eb5d9f6f11849f0fa310ee4
Sasha Zmts <sa.zmts@gmail.com>
2019-06-20 11:36:04 +0300

    minor jwt service error handling

--- services/auth/jwtService.js
+++ services/auth/jwtService.js
@@ -32,7 +32,7 @@ module.exports.sign = (playload, SECRET, options) => {
 
   return new Promise((resolve, reject) => {
     jwt.sign(playload, SECRET, options, (error, token) => {
-      if (error) return reject(new ErrorWrapper({ ...errorCodes.TOKEN_NOT_SIGNED }))
+      if (error) return reject(new ErrorWrapper({ ...errorCodes.TOKEN_NOT_SIGNED, message: error.message }))
       return resolve(token)
     })
   })
