cecb49c392aee98c9557a5f9b6d09afc925626e6
Sasha Zmts <sa.zmts@gmail.com>
2019-07-28 14:38:26 +0300

    config

--- config/app.js
+++ config/app.js
@@ -3,6 +3,7 @@ const BaseConfig = require('../core/BaseConfig')
 class AppConfig extends BaseConfig {
   constructor () {
     super()
+    this.nodeEnv = this.set('NODE_ENV', v => ['development', 'production'].includes(v), 'development')
     this.port = this.set('APP_PORT', this.joi.number().port().required(), 5555)
     this.host = this.set('APP_HOST', this.joi.string().required(), 'localhost')
     this.name = this.set('APP_NAME', this.joi.string().required(), 'SupraAPI')
