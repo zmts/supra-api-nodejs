5513e928846a096693430d736c190547689e72b4
Sasha Zmts <sa.zmts@gmail.com>
2019-09-03 12:59:19 +0300

    minor

--- core/index.js
+++ core/index.js
@@ -1,16 +1,18 @@
 const errorCodes = require('./lib/errorCodes')
 const assert = require('./lib/assert')
 const clients = require('./lib/clients')
+
 const BaseConfig = require('./lib/BaseConfig')
 const BaseDAO = require('./lib/BaseDAO')
 const BaseMiddleware = require('./lib/BaseMiddleware')
-const Logger = require('./lib/Logger')
 const BaseModel = require('./lib/BaseModel')
+
 const ErrorWrapper = require('./lib/ErrorWrapper')
 const InMemoryCache = require('./lib/InMemoryCache')
 const Rule = require('./lib/Rule')
 const SentryCatch = require('./lib/SentryCatch')
 const Server = require('./lib/Server')
+const Logger = require('./lib/Logger')
 
 module.exports = {
   errorCodes,
