bf20fe02e70c9194d80170787c0deae8108fc38b
Sasha Zmts <sa.zmts@gmail.com>
2019-10-21 11:26:53 +0300

    minor comment

--- middlewares/CheckAccessTokenMiddleware.js
+++ middlewares/CheckAccessTokenMiddleware.js
@@ -40,8 +40,7 @@ class CheckAccessTokenMiddleware extends BaseMiddleware {
             if (error.code === errorCodes.TOKEN_EXPIRED.code) {
               /**
                * pass request if token is not valid
-               * in that case current user data in registry will be empty
-               * and security service will be consider that request as guest request
+               * in this case security service will consider that request as anonymous request
                */
               next()
             } else {
