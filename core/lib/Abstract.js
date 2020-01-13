8e0beab6212e92fa6c67958585332b448e3d429e
Sasha Zmts <sa.zmts@gmail.com>
2020-01-13 22:06:42 +0200

    lib/abstract class

---
+++ core/lib/Abstract.js
@@ -0,0 +1,28 @@
+const { Assert: assert } = require('./assert')
+
+class Abstract {
+  __abstractField (ctx, fieldName) {
+    assert.object(ctx, { required: true })
+    assert.string(fieldName, { notEmpty: true })
+
+    if (!ctx[fieldName]) {
+      throw new Error(`${ctx.constructor.name}: ${fieldName} field not implemented`)
+    }
+  }
+
+  __abstractMethod (ctx, methodName, argsQuantity) {
+    assert.object(ctx, { required: true })
+    assert.string(methodName, { notEmpty: true })
+    assert.integer(argsQuantity, { required: true, positive: true })
+
+    if (!ctx[methodName]) {
+      throw new Error(`${ctx.constructor.name}: ${methodName} method not implemented`)
+    }
+
+    if (ctx[methodName].length !== argsQuantity) {
+      throw new Error(`${ctx.constructor.name}: ${methodName} method invalid arguments length`)
+    }
+  }
+}
+
+module.exports = { Abstract }
