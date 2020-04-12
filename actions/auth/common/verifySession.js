7bff64656764992e7a380d29c1b26ad5e2248703
Sasha Zmts <sa.zmts@gmail.com>
2020-04-12 22:55:53 +0300

    minor

--- actions/auth/common/verifySession.js
+++ actions/auth/common/verifySession.js
@@ -1,18 +1,16 @@
 const { errorCodes, AppError, assert } = require('supra-core')
 const { SessionEntity } = require('./SessionEntity')
 
-function verifySession (session, newFingerprint) {
-  assert.instanceOf(session, SessionEntity)
+function verifySession (oldSession, newFingerprint) {
+  assert.instanceOf(oldSession, SessionEntity)
   assert.string(newFingerprint, { notEmpty: true })
 
   return new Promise((resolve, reject) => {
-    const sessionExpiresIn = session.expiresIn
-    const oldFingerprint = session.fingerprint
     const nowTime = new Date().getTime()
 
-    if (nowTime > sessionExpiresIn) return reject(new AppError({ ...errorCodes.SESSION_EXPIRED }))
+    if (nowTime > oldSession.expiresIn) return reject(new AppError({ ...errorCodes.SESSION_EXPIRED }))
     // if (oldIp !== newIp) return reject(new AppError({ ...errorCodes.INVALID_SESSION })) // for best security
-    if (oldFingerprint !== newFingerprint) return reject(new AppError({ ...errorCodes.INVALID_SESSION }))
+    if (oldSession.fingerprint !== newFingerprint) return reject(new AppError({ ...errorCodes.INVALID_SESSION }))
     return resolve()
   })
 }
