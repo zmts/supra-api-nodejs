fb9ae9aedb3ffe6e14547df87ab104d28057fd1e
Sasha Zmts <sa.zmts@gmail.com>
2020-08-25 11:36:47 +0300

    set json/urlencoded limit

--- core/lib/Server.js
+++ core/lib/Server.js
@@ -8,27 +8,28 @@ const { BaseMiddleware } = require('./BaseMiddleware')
 const { AbstractLogger } = require('./AbstractLogger')
 
 class Server {
-  constructor ({ port, host, controllers, middlewares, errorMiddleware, cookieSecret, logger }) {
+  constructor ({ port, host, controllers, middlewares, errorMiddleware, cookieSecret, reqLimit = '5mb', logger }) {
     assert.integer(port, { required: true, min: 1000 })
     assert.string(host, { required: true, notEmpty: true })
     assert.array(controllers, { required: true, notEmpty: true, message: 'controllers param expects not empty array' })
     assert.array(middlewares, { required: true, notEmpty: true, message: 'middlewares param expects not empty array' })
     assert.instanceOf(errorMiddleware.prototype, BaseMiddleware)
     assert.string(cookieSecret)
+    assert.string(reqLimit)
     assert.instanceOf(logger, AbstractLogger)
 
     logger.info('Server start initialization...')
-    return start({ port, host, controllers, middlewares, ErrorMiddleware: errorMiddleware, cookieSecret, logger })
+    return start({ port, host, controllers, middlewares, ErrorMiddleware: errorMiddleware, cookieSecret, reqLimit, logger })
   }
 }
 
-function start ({ port, host, controllers, middlewares, ErrorMiddleware, cookieSecret, logger }) {
+function start ({ port, host, controllers, middlewares, ErrorMiddleware, cookieSecret, reqLimit, logger }) {
   return new Promise(async (resolve, reject) => {
     const app = express()
 
     if (process.env.NODE_ENV !== 'production') app.use(morganLogger('dev'))
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: false }))
+    app.use(express.json({ limit: reqLimit }))
+    app.use(express.urlencoded({ extended: false, limit: reqLimit }))
     app.use(cookieParser(cookieSecret))
     app.use(express.static(path.join(__dirname, 'public')))
 
