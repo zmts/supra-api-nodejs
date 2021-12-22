ad1ce4128e6308a5ddff8d410fb180169f9cbe93
Sasha Zmts <sa.zmts@gmail.com>
2021-12-22 21:53:59 +0200

    minor: upd config

--- config/folders.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  rootDir: process.env.PWD,
-  publicDir: process.env.PWD + '/public',
-  photoDir: process.env.PWD + '/public/photos',
-  userfilesDir: process.env.PWD + '/public/userfiles'
-}
--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
 const app = require('./app')
 const knex = require('./knex')
-const folders = require('./folders')
 const token = require('./token')
 const email = require('./email')
 const s3 = require('./s3')
@@ -29,7 +28,6 @@ function rootInit () {
 module.exports = {
   app,
   knex,
-  folders,
   token,
   email,
   s3,
